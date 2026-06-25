1f4f787e23785906b23b35e1e36a662581cb44a9
meshivamguptaa <shivammgupta0408@gmail.com>
2026-06-25 18:04:46 +0530

    added user details from fetch

--- fetch/index.js
+++ fetch/index.js
@@ -1,12 +1,28 @@
 const button= document.getElementById("btn")
-
+const text= document.getElementById("output")
 button.addEventListener("click", async () => {
 
     try{
-        console.log("Loading...")
-        const response = await fetch(https://jsonplaceholder.typicode.com/users/1)
-        const result = await response.json()
+        text.textContent= "Loading..."
+        const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+
+        //when server respons with 404 not found
+        if(!response.ok){
+            throw new Error("failed to fetch data")
+        }
+
+        const user = await response.json()
+        console.log(user)
+
         
+        text.textContent = `
+            Name: ${user.name}
+            Email: ${user.email}
+            City: ${user.address.city}
+`
+    }
+    catch(error){
+        text.textContent= "Something went wrong"
 
     }
 
